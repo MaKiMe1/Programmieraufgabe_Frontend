5ba0ba56560b4fb4c0002138cf4fa7315d22223c
Alexander Schneider <Ethirras@gmail.com>
2023-07-03 15:31:08 +0200

    Changed name in Teachermask

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CourseView from "../views/CourseView.vue";
 
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
